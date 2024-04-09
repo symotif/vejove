8b60ec4ee40d06a9ee83ec40c7a62a8f21d39029
loganynguyen <logan.y.nguyen@gmail.com>
2024-04-08 20:53:36 -0400

    polish

--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -1,3 +1,4 @@
 <div class="pt-16">
+    <h1 class="text-xl font-bold">Learn</h1>
     <h1>Learn lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus tempora ea eligendi laudantium quisquam aliquam quia, unde tempore qui maiores expedita sapiente commodi labore, beatae nemo perspiciatis nulla necessitatibus enim.</h1>
 </div>
--- src/routes/zen/+page.svelte
+++ src/routes/zen/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="pt-16 flex flex-col justify-center items-center">
-    <h1>Calm</h1>
+    <h1 class="text-xl font-bold">Zen</h1>
     <Button type="submit">Press to begin</Button>
     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus tempora ea eligendi laudantium quisquam aliquam quia, unde tempore qui maiores expedita sapiente commodi labore, beatae nemo perspiciatis nulla necessitatibus enim.</h1>
 </div>
