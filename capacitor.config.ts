7f1c0fd8b8d4d73af8d0e2f23c81747f039edb7d
loganynguyen <logan.y.nguyen@gmail.com>
2024-04-10 22:59:29 -0400

    uncommented server code in cap config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,12 +7,12 @@ const config: CapacitorConfig = {
   bundledWebRuntime: false,
 
   // comment out these options below when you are running in production. this is for hmr when developing!
-  // server: {
-  //   url: 'http://192.168.0.62:5173',
-  //   cleartext: true,
-  //   //allowNavigation: ['*'],
-  //   //androidScheme: 'https'
-  // },
+  server: {
+    url: 'http://192.168.0.62:5173',
+    cleartext: true,
+    //allowNavigation: ['*'],
+    //androidScheme: 'https'
+  },
 };
 
 export default config;
